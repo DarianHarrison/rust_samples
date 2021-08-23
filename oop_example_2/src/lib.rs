74c717193bd65c3da8c7910b95685cabbaaecf71
Darian Harrison <darianharrison89@gmail.com>
2021-08-23 09:39:09 -0500

    oop4

--- oop_example_2/src/lib.rs
+++ oop_example_2/src/lib.rs
@@ -4,35 +4,50 @@
 
 // We know we need a public Post struct that holds some content, 
 // so we’ll start with the definition of the struct and an associated public new function to create an instance of Post
-
 // We’ll also make a private State trait. 
 // Then Post will hold a trait object of Box<dyn State> inside an Option<T> in a private field named state. 
 
-
-
-
-
 pub struct Post {
-    state: Option<Box<dyn State>>,
+    content: String,
+}
+
+pub struct DraftPost {
     content: String,
 }
 
 impl Post {
-    pub fn new() -> Post {
-        Post {
-            state: Some(Box::new(Draft {})),
+    pub fn new() -> DraftPost {
+        DraftPost {
             content: String::new(),
         }
     }
 
-    // Implementing the add_text method to add text to a post’s content.
-    pub fn add_text(&mut self, text: &str) { // The add_text method takes a mutable reference to self, because we’re changing the Post instance that we’re calling add_text on. 
-        self.content.push_str(text);
+    pub fn content(&self) -> &str {
+        &self.content
     }
 }
 
-trait State {} // The State trait defines the behavior shared by different post states, and the Draft, PendingReview, and Published states will all implement the State trait. 
+impl DraftPost {
+    // --snip--
+    pub fn add_text(&mut self, text: &str) {
+        self.content.push_str(text);
+    }
 
-struct Draft {}
+    pub fn request_review(self) -> PendingReviewPost {
+        PendingReviewPost {
+            content: self.content,
+        }
+    }
+}
 
-impl State for Draft {}
+pub struct PendingReviewPost {
+    content: String,
+}
+
+impl PendingReviewPost {
+    pub fn approve(self) -> Post {
+        Post {
+            content: self.content,
+        }
+    }
+}
