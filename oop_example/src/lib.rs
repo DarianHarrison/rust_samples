f6b4bd82e537b9d24160d78a123c7b14af32755f
Darian Harrison <darianharrison89@gmail.com>
2021-08-23 00:51:27 -0500

    oop2

--- oop_example/src/lib.rs
+++ oop_example/src/lib.rs
@@ -1,16 +1,33 @@
-// define a trait named Mlp with one method named draw:
-
-pub trait Mlp {
-    fn draw(&self);
-    // tuple (source,dest,weight,boolean), if boolean is 1 source to destination is directed, else we just worry about edge connections
-    // fn draw(&self, numLayers, inputDim, hiddenDim, outputDim);
-
+// define a trait named Draw with one method named draw:
+pub trait Draw {
+    fn draw(&self); // 
 }
 
-        
-// num_layers: number of layers in the neural networks (EXCLUDING the input layer). If num_layers=1, this reduces to linear model.
-// input_dim: dimensionality of input features
-// hidden_dim: dimensionality of hidden units at ALL layers
-// output_dim: number of classes for prediction
-// device: which device to use
-        
+pub struct Screen { // a struct named Screen that holds a vector named components. 
+    pub components: Vec<Box<dyn Draw>>, // This vector is of type Box<dyn Draw>, which is a trait object; it's a stand-in for any type inside a Box that implements the Draw trait.
+}
+
+  
+// On the Screen struct, we’ll define a method named run that will call the draw method on each of its components
+impl Screen {
+    pub fn run(&self) {
+        for component in self.components.iter() {
+            component.draw();
+        }
+    }
+}
+
+// A Button struct that implements the Draw trait
+pub struct Button {
+    pub width: u32,
+    pub height: u32,
+    pub label: String,
+}
+
+impl Draw for Button {
+    fn draw(&self) {
+        // code to actually draw a button
+    }
+}
+
+// The Self keyword is an alias for the type we’re implementing the traits or methods on.
